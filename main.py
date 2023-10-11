cfeac079a6370dd9100ad70d38cfce38e5fc2965
ryand <s1146917@student.hsleiden.nl>
2023-10-11 16:23:48 +0200

    redo, new: paths_screen; old: make_path_choice

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from tkinter import Tk, Label
-from PIL import Image, ImageTk
 from Splash_screen import make_splash_screen
+from paths_screen import generate_screen
+from PIL import Image, ImageTk
 
 
 def kill_all_children(venster):
     for widget in venster.winfo_children():
         widget.destroy()
-
+    generate_background(venster)
 
 def generate_background(root):
     original_image = Image.open(r"Images/background.png")
@@ -16,15 +17,42 @@ def generate_background(root):
     background.image = background_image
     background.place(x=0, y=0)
 
-
 def main():
     root = Tk()
-    root.title("Boromir")
     root.geometry("1920x1080")
+    root.title("Boromir")
+    root.state('zoomed')
+
+
+    option1_dict = {
+        "text": "Option 1 tekst",
+        "action": "action_code here"
+    }
+    option2_dict = {
+        "text": "Option 2 tekst",
+        "action": "action_code here"
+    }
+    option3_dict = {
+        "text": "Option 3 tekst",
+        "action": "action_code here"
+    }
+    option4_dict = {
+        "text": "Option 4 tekst\n and more",
+        "action": "action_code here"
+    }
+
+    options_list = [option4_dict, option3_dict, option2_dict, option1_dict]
+
+    screen_dict = {
+        "question": "put question here",
+        "options": options_list
+    }
+    root.winfo_screenheight()
+    root.winfo_height()
+    generate_background(root)
     make_splash_screen(root)
 
     root.mainloop()
-
 
 if __name__ == '__main__':
     main()
---
+++ paths_screen.py
@@ -0,0 +1,49 @@
+from tkinter import Label, Frame
+from PIL import Image, ImageTk
+
+
+def generate_screen(venster, screen_dict):
+    global root
+    root = venster
+    from main import kill_all_children
+    kill_all_children(root)
+
+    #image_settings_characters
+    character_imagesize_x = 400
+    character_imagesize_y = 800
+
+    original_image = Image.open(r"Images/stickman_echt.png")
+    resized_image = original_image.resize((character_imagesize_x, character_imagesize_y), Image.LANCZOS)
+    character_slot_1_image = ImageTk.PhotoImage(resized_image)
+
+
+    character_slot_1 = Label(root, image=character_slot_1_image, borderwidth=5, relief="raised", background="#88cffa")
+    character_slot_1.image = character_slot_1_image
+    character_slot_1.pack(anchor="nw", side="left", pady=100, padx=(5, 5))
+
+    character_slot_2 = Label(root, image=character_slot_1_image, borderwidth=5, relief="raised", background="#88cffa")
+    character_slot_2.image = character_slot_1_image
+    character_slot_2.pack(anchor="ne", side="right", pady=100, padx=(5, 5))
+    create_buttons(screen_dict)
+
+
+def create_buttons(screen_dict):
+    frame1 = Frame(root, width=1000, height=800, borderwidth=5, relief="raised", background="#88cffa")
+    frame1.pack(anchor="center", pady=100)
+    frame1.propagate(0)
+
+    frame2 = Frame(frame1, borderwidth=5, relief="raised", height=600, background="#88cffa")
+    frame2.pack(side="top", anchor="center", fill="x")
+    frame2.propagate(0)
+
+    min_space_label = Label(frame2, width=10, height=10, bg="black")
+    min_space_label.pack(anchor="n", fill="x")
+
+    option_list = screen_dict['options']
+    for option in option_list:
+        option_button = Label(frame2, text=option['text'], height=5, pady=1, padx=1, borderwidth=1, relief="raised", anchor="w", justify="left", background="#88cffa")
+        #bind here
+        option_button.pack(anchor="w", pady=1, padx=1, fill="x")
+
+    question_label = Label(frame1, text=screen_dict['question'], anchor="center", compound="center", borderwidth=2, relief="sunken")
+    question_label.pack(expand=1)
