517036a2a4ae72ac4ca4a5fd21138e827e6287ea
ryand <s1146917@student.hsleiden.nl>
2023-10-11 08:47:55 +0200

    Func, kill all children. Setup main window

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
+from tkinter import Tk
+from Splash_screen import make_splash_screen
+
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def kill_all_children(venster):
+    for widget in venster.winfo_children():
+        widget.destroy()
 
+def main():
+    root = Tk()
+    root.geometry("1920x1080")
+    root.title("Boromir")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    root.mainloop()
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-kiikikkikiij
