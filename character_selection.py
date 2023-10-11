06882e169991c9aa8119ec7c091ed54bd4082574
R. Waaning <r.waaning@umail.leidenuniv.nl>
2023-10-11 14:08:14 +0200

    Added pictures to the templates for characters

--- character_selection.py
+++ character_selection.py
@@ -21,31 +21,37 @@ def make_character_selection_screen(venster):
     from main import kill_all_children
     kill_all_children(venster)
 
-    original_image = Image.open(r"Images/stickman_echt.png")
-    voorbeeld_image = ImageTk.PhotoImage(original_image)
+    menu_button_image_proto = Image.open(r"Images/testbutton.png")
+    menu_button_image = ImageTk.PhotoImage(menu_button_image_proto)
 
-    original_image = Image.open(r"Images/testbutton.png")
-    voorbeeld_image2 = ImageTk.PhotoImage(original_image)
+    template_character1_image_proto = Image.open(r"Images/hobbit_male.png")
+    template_character1_image = template_character1_image_proto.resize((200, 200), Image.LANCZOS)
+    template_character1_image = ImageTk.PhotoImage(template_character1_image)
+    template_character1_button = Label(venster, text="Template character 1 \n Male Hobbit", image=template_character1_image, compound="bottom")
+    template_character1_button.image = template_character1_image
+    template_character1_button.bind("<Button-1>", lambda click_event: goto_adventure_selection_screen(venster))
+    template_character1_button.place(relx=0.3, rely=0.4, anchor="center")
 
+    template_character2_image_proto = Image.open(r"Images/elf_male.png")
+    template_character2_image = template_character2_image_proto.resize((200, 200), Image.LANCZOS)
+    template_character2_image = ImageTk.PhotoImage(template_character2_image)
+    template_character2_button = Label(venster, text="Template character 1 \n Male Elf", image=template_character2_image, compound="bottom")
+    template_character2_button.image = template_character2_image
+    template_character2_button.bind("<Button-1>", lambda click_event: goto_adventure_selection_screen(venster))
+    template_character2_button.place(relx=0.5, rely=0.4, anchor="center")
 
+    template_character3_image_proto = Image.open(r"Images/dwarf_male.png")
+    template_character3_image = template_character3_image_proto.resize((200, 200), Image.LANCZOS)
+    template_character3_image = ImageTk.PhotoImage(template_character3_image)
+    template_character3_button = Label(venster, text="Template character 1 \n Male Dwarf", image=template_character3_image, compound="bottom")
+    template_character3_button.image = template_character3_image
+    template_character3_button.bind("<Button-1>", lambda click_event: goto_adventure_selection_screen(venster))
+    template_character3_button.place(relx=0.7, rely=0.4, anchor="center")
 
-    start_button = Label(venster, text="Template character 1", image=voorbeeld_image, compound="bottom")
-    start_button.image = voorbeeld_image
-    start_button.bind("<Button-1>", lambda click_event: goto_adventure_selection_screen(venster))
-    start_button.place(relx=0.3, rely=0.4, anchor="center")
-
-    start_button2 = Label(venster, text="Template character 2", image=voorbeeld_image, compound="bottom")
-    start_button2.image = voorbeeld_image
-    start_button2.bind("<Button-1>", lambda click_event: goto_adventure_selection_screen(venster))
-    start_button2.place(relx=0.5, rely=0.4, anchor="center")
-
-    start_button3 = Label(venster, text="Template character 3", image=voorbeeld_image, compound="bottom")
-    start_button3.image = voorbeeld_image
-    start_button3.bind("<Button-1>", lambda click_event: goto_adventure_selection_screen(venster))
-    start_button3.place(relx=0.7, rely=0.4, anchor="center")
-
-    start_button4 = Label(venster, text="User characters", image=voorbeeld_image2, compound="center")
-    start_button4.image = voorbeeld_image2
+    start_button4 = Label(venster, text="User created characters", image=menu_button_image, compound="center")
+    start_button4.image = menu_button_image
     start_button4.bind("<Button-1>", lambda click_event: goto_character_creation(venster))
     start_button4.place(relx=0.5, rely=0.6, anchor="center")
+
+
 
