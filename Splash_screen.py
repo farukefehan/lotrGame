2d609c27dd51b1651ee21b55bc191433a4b5a51b
R. Waaning <r.waaning@umail.leidenuniv.nl>
2023-10-11 10:12:46 +0200

    Added images to folder and refactored code to use folder path

--- Splash_screen.py
+++ Splash_screen.py
@@ -1,6 +1,5 @@
 from tkinter import Label
 from PIL import Image, ImageTk
-# Hoi Thijs
 
 
 def goto_screen_select_character(venster):
@@ -12,11 +11,11 @@ def make_splash_screen(venster):
     from main import kill_all_children
     kill_all_children(venster)
 
-    orignial_image = Image.open(r"testbutton.png")
+    orignial_image = Image.open(r"Images/testbutton.png")
 
     voorbeeld_image = ImageTk.PhotoImage(orignial_image)
 
-    start_button = Label(venster, text="START", height=80, width=200, image=voorbeeld_image, compound="center")
+    start_button = Label(venster, text="Choose your character", height=80, width=200, image=voorbeeld_image, compound="center")
     start_button.image = voorbeeld_image
 
     start_button.bind("<Button-1>", lambda click_event: goto_screen_select_character(venster))
--- character_selection.py
+++ character_selection.py
@@ -8,4 +8,18 @@ def make_character_selection_screen(venster):
 
     label_2 = Label(venster, text="character selection screen")
 
+    original_image = Image.open(r"Images/stickman_echt.png")
+
+    voorbeeld_image = ImageTk.PhotoImage(original_image)
+
+    # height = 700, width = 493,
+
+    start_button = Label(venster, image=voorbeeld_image,
+                         compound="center")
+    start_button.image = voorbeeld_image
+
+    # start_button.bind("<Button-1>", lambda click_event: goto_screen_select_character(venster))
+
+    start_button.pack(anchor="n", pady="100")
+
     label_2.pack()
