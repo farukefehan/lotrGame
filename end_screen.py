ce39e9eedb22be07ca0d6c5fef79ab336fae89b5
R. Waaning <r.waaning@umail.leidenuniv.nl>
2023-10-11 14:48:33 +0200

    Optimized functions

--- end_screen.py
+++ end_screen.py
@@ -2,38 +2,45 @@ from PIL import Image, ImageTk
 from tkinter import Label
 
 
+def clear_page(venster):
+    from main import kill_all_children
+    kill_all_children(venster)
+
+
 def goto_splash_screen(venster):
     from Splash_screen import make_splash_screen
     make_splash_screen(venster)
 
-def make_end_screen(venster, player_has_died):
-    from main import kill_all_children
-    kill_all_children(venster)
 
-    if player_has_died:
-        negative_end_image = Image.open(r"Images/you_died.png")
-        original_image_negative = negative_end_image.resize((1200, 400), Image.LANCZOS)
-        voorbeeld_image = ImageTk.PhotoImage(original_image_negative)
+def print_adventure_result(venster, image):
+    end_image = Image.open(image)
+    original_image = end_image.resize((1200, 400), Image.LANCZOS)
+    voorbeeld_image = ImageTk.PhotoImage(original_image)
 
-        start_button = Label(venster, image=voorbeeld_image, compound="bottom")
-        start_button.image = voorbeeld_image
-        start_button.place(relx=0.5, rely=0.3, anchor="center")
-    else:
-        positive_end_image = Image.open(r"Images/the_end.png")
-        original_image_positive = positive_end_image.resize((1200, 400), Image.LANCZOS)
-        voorbeeld_image = ImageTk.PhotoImage(original_image_positive)
-
-        start_button = Label(venster, image=voorbeeld_image, compound="bottom")
-        start_button.image = voorbeeld_image
-        start_button.place(relx=0.5, rely=0.3, anchor="center")
-
-    original_image2 = Image.open(r"Images/home_button.png")
-    original_image2 = original_image2.resize((200, 200), Image.LANCZOS)
-    voorbeeld_image2 = ImageTk.PhotoImage(original_image2)
-
-    start_button2 = Label(venster, image=voorbeeld_image2, compound="bottom")
-    start_button2.image = voorbeeld_image2
-    start_button2.bind("<Button-1>", lambda click_event: goto_splash_screen(venster))
-    start_button2.place(relx=0.5, rely=0.7, anchor="center")
+    start_button = Label(venster, image=voorbeeld_image, compound="bottom")
+    start_button.image = voorbeeld_image
+    start_button.place(relx=0.5, rely=0.3, anchor="center")
+    make_home_button(venster)
 
 
+def make_home_button(venster):
+    home_button_image_proto = Image.open(r"Images/home_button.png")
+    home_button_image_proto = home_button_image_proto.resize((200, 200), Image.LANCZOS)
+    home_button_image = ImageTk.PhotoImage(home_button_image_proto)
+
+    end_button2 = Label(venster, image=home_button_image, compound="bottom")
+    end_button2.image = home_button_image
+    end_button2.bind("<Button-1>", lambda click_event: goto_splash_screen(venster))
+    end_button2.place(relx=0.5, rely=0.7, anchor="center")
+
+
+def make_end_screen_dead(venster):
+    clear_page(venster)
+    image_for_result_path = r"Images/you_died.png"
+    print_adventure_result(venster, image_for_result_path)
+
+
+def make_end_screen_success(venster):
+    clear_page(venster)
+    image_for_result_path = r"Images/the_end.png"
+    print_adventure_result(venster, image_for_result_path)
