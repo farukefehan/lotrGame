8f00dbfdc1a1e08d3d82c4345b2adde04cb32975
rickd <s1145065@student.hsleiden.nl>
2023-10-11 11:09:49 +0200

    Added added image for adventure 1

--- Avonturen_selection.py
+++ Avonturen_selection.py
@@ -8,17 +8,22 @@ def make_adventure_selection_screen(venster):
     adventure2 = Label(venster, text="Avontuur 2")
     adventure3 = Label(venster, text="Avontuur 3")
 
-    adventure1_image_proto = Image.open(r"Images/adventure1_proto.jpg")
+    adventure1_image_proto = Image.open(r"Images/adventure1_proto.png")
 
-    adventure1_image = ImageTk.PhotoImage(adventure1_image_proto)
+    adventure1_image = adventure1_image_proto.resize((400, 400), Image.LANCZOS)
+    adventure1_image = ImageTk.PhotoImage(adventure1_image)
 
     adventure1_button = Label(venster, image=adventure1_image,
                          compound="left")
+    adventure1_button.image = adventure1_image
     adventure2_button = Label(venster,
                          compound="center")
     adventure3_button = Label(venster,
                          compound="right")
 
+    adventure1_button.pack()
     adventure1.pack()
     adventure2.pack()
     adventure3.pack()
+
+    #adventure1_button.bind("<Button-1>", lambda click_event: )
